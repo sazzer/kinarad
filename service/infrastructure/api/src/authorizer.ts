1e983fa6fdae84cec0d70604613fa35b4bd258a8
Graham Cox <graham@grahamcox.co.uk>
2021-02-13 11:33:49 +0000

    Deny the incoming request if a non-bearer token is provided

--- service/infrastructure/api/src/authorizer.ts
+++ service/infrastructure/api/src/authorizer.ts
@@ -3,9 +3,35 @@ import {
   APIGatewayRequestAuthorizerEvent,
 } from "aws-lambda";
 
+/** The prefix for the bearer token */
+const BEARER_PREFIX = "Bearer ";
+
+/**
+ * The actual handler for the authorizer.
+ * @param event The incoming event
+ */
 export async function handler(
   event: APIGatewayRequestAuthorizerEvent
 ): Promise<APIGatewayAuthorizerResult> {
+  const authorization = event.headers?.authorization;
+
+  if (authorization === undefined) {
+    // No header, so allow the request with no claims
+    return generatePolicy(event.methodArn, true);
+  } else if (!authorization.startsWith(BEARER_PREFIX)) {
+    // The header is present, but it's not a bearer token, so deny the request
+    return generatePolicy(event.methodArn, false);
+  } else {
+    return generatePolicy(event.methodArn, true);
+  }
+}
+
+/**
+ * Generate an IAM policy for this request
+ * @param resource The ARN of the resource to generate the policy for
+ * @param allowed Whether the request is allowed or not
+ */
+function generatePolicy(resource: string, allowed: boolean) {
   return {
     principalId: "",
     policyDocument: {
@@ -13,14 +39,11 @@ export async function handler(
       Statement: [
         {
           Action: "execute-api:Invoke",
-          Effect: "Allow",
-          Resource: event.methodArn,
+          Effect: allowed ? "Allow" : "Deny",
+          Resource: resource,
         },
       ],
     },
-    context: {
-      hello: "world",
-      answer: 42,
-    },
+    context: {},
   };
 }
