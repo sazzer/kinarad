63e2a0a73f1ac79746f7da8a6edeedadc6c6107e
Graham Cox <graham@grahamcox.co.uk>
2021-02-15 20:23:04 +0000

    Set up linting for the users module

--- service/modules/users/src/lambdas/get/index.ts
+++ service/modules/users/src/lambdas/get/index.ts
@@ -1,8 +1,4 @@
-import {
-  APIGatewayProxyEventV2,
-  APIGatewayProxyResultV2,
-  Context,
-} from "aws-lambda";
+import { APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from 'aws-lambda';
 
 /**
  * Handler for retrieving a single user by ID.
@@ -10,14 +6,11 @@ import {
  * @param event The incoming event
  * @param context The context to operate under
  */
-export async function handler(
-  event: APIGatewayProxyEventV2,
-  context: Context
-): Promise<APIGatewayProxyResultV2> {
+export async function handler(event: APIGatewayProxyEventV2): Promise<APIGatewayProxyResultV2> {
   return {
     statusCode: 200,
     headers: {
-      "content-type": "application/json",
+      'content-type': 'application/json',
     },
     body: JSON.stringify({
       name: event.pathParameters?.userId,
